2b636400bb6cb10d806dd8f5b409c0322ebc851f
zhangyj <richard.yujia.zhang@gmail.com>
2024-05-21 15:18:48 +0800

    feat: 删除模板中不需要的内容

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
-  npmClient: 'yarn',
+  routes: [{ path: "/", component: "index" }],
+  npmClient: "yarn",
 });
