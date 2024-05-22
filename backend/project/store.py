346a05461e72e26c089880483eade4d6750f7fc3
zhangyj <richard.yujia.zhang@gmail.com>
2024-05-22 16:50:22 +0800

    feat: 修改文件存储

--- backend/project/store.py
+++ backend/project/store.py
@@ -5,7 +5,7 @@ import datetime
 # 存储一个文件，返回相对路径
 def store_file(file):
     now = datetime.datetime.now()
-    folder = now.strftime('%Y%m')
+    folder = f"data/{now.strftime('%Y%m')}"
     timestamp = now.strftime('%d%H%M%S')
     os.makedirs(folder, exist_ok=True)
     path = f'{folder}/{timestamp}-{file.filename}'
