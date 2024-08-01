6d5e83d9764df9585f1aecfd0a5ccc8bef1bc7b6
zhangyj <richard.yujia.zhang@gmail.com>
2024-08-01 10:35:03 +0800

    feat: 检索页面主体卡片

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card } from "antd";
+import {} from "@ant-design/icons";
+import { useRequest } from "ahooks";
+import { fetchTagsRequest } from "../service";
+import styles from "./index.css";
 
 const DashboardPage: React.FC = () => {
-  return <div>情报检索</div>;
+  // 各种网络请求
+  const { runAsync: fetchTags, data: tags } = useRequest(fetchTagsRequest);
+
+  return <Card className={styles.mainCard} title="情报检索"></Card>;
 };
 
 export default DashboardPage;
