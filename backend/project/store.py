c790e86f38941e60f957df406edd00d1304c621a
zhangyj <richard.yujia.zhang@gmail.com>
2024-08-02 10:14:14 +0800

    fix: 数据库中存储相对路径

--- backend/project/store.py
+++ backend/project/store.py
@@ -8,16 +8,20 @@ from . import config
 def store_file(file):
     now = datetime.datetime.now()
     root_folder = config['store-root']
-    folder = os.path.join(root_folder, now.strftime('%Y%m'))
+    rel_folder = now.strftime('%Y%m')
+    abs_folder = os.path.join(root_folder, rel_folder)
     timestamp = now.strftime('%d%H%M%S')
-    os.makedirs(folder, exist_ok=True)
-    path = f'{folder}/{timestamp}-{file.filename}'
-    file.save(path)
+    os.makedirs(abs_folder, exist_ok=True)
+    rel_path = f'{rel_folder}/{timestamp}-{file.filename}'
+    abs_path = f'{abs_folder}/{timestamp}-{file.filename}'
+    file.save(abs_path)
 
-    return path
+    return rel_path
 
 
 # 删除一个文件
 def del_file(rel_path):
-    if os.path.exists(rel_path):
-        os.remove(rel_path)
+    root_folder = config['store-root']
+    abs_path = os.path.join(root_folder, rel_path)
+    if os.path.exists(abs_path):
+        os.remove(abs_path)
